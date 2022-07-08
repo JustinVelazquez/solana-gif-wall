942a5334520311603d9ee996a5056046d088c03e
Justin V <justinv4994@gmail.com>
2022-07-08 19:50:54 -0400

    added phantom wallet check

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
-
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+import { useEffect } from 'react';
 
 const App = () => {
+  // const checkIfWalletIsConnected = async () => {
+  //   try {
+  //     const { solana } = window;
+  //     if (solana) {
+  //       if (solana.isPhantom) {
+  //         console.log('Phantom Wallet found!');
+  //       } else {
+  //         alert('Solana object not found! Download the Phantom Wallet!');
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+ 
   return (
     <div className="App">
       <div className="container">
@@ -17,12 +52,12 @@ const App = () => {
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
+          {/* <a
             className="footer-text"
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built on @${TWITTER_HANDLE}`}</a> */}
         </div>
       </div>
     </div>
